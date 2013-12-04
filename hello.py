4153a3883fc05a3271937fc0a21ab753e9a16531
Nelson Sachse <nelson@madebymany.co.uk>
2013-12-04 11:45:36 +0000

    Changed templates. Added app routes.

--- hello.py
+++ hello.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
 app = Flask(__name__)
 
-@app.route("/hello")
+
+@app.route("/")
 def hello():
-    return "Hello World!"
+    return "<html><body><h1>Welcome</h1></body></html>"
+
+@app.route("/about")
+def about():
+	return "About Page"
+
+@app.route('/login')
+@app.route('/login/<name>')
+def login(name=None):
+    return render_template('show_login.html', name=name)
 
 if __name__ == "__main__":
-    app.run()
+	app.debug = True
+	app.run()
